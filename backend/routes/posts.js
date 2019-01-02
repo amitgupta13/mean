71db9604c464d0ca7f379d63f8d01b34d65b6843
Amit Gupta <vikas.s@daffodilsw.com>
2019-01-02 11:44:23 +0530

    test

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const Post = require("../models/post");
 const multer = require('multer');
+const auth = require('../middleware/auth');
 const router = express.Router();
 
 const MIME_TYPE_MAP = {
@@ -25,7 +26,7 @@ const storage = multer.diskStorage({
   }
 });
 
-router.post("/", multer({storage:storage}).single('image'), (req, res, next) => {
+router.post("/", auth, multer({storage:storage}).single('image'), (req, res, next) => {
     const url = `${req.protocol}://${req.get('host')}`;
     const post = new Post({
       title: req.body.title,
@@ -44,7 +45,7 @@ router.post("/", multer({storage:storage}).single('image'), (req, res, next) => 
     });
   });
   
-  router.put('/:id', multer({storage:storage}).single('image'), (req, res, next)=>{
+  router.put('/:id', auth, multer({storage:storage}).single('image'), (req, res, next)=>{
     let imagePath = req.body.imagePath;
     if(req.file){
       const url = `${req.protocol}://${req.get('host')}`;
@@ -97,7 +98,7 @@ router.post("/", multer({storage:storage}).single('image'), (req, res, next) => 
     })
   });
   
-  router.delete("/:id", (req, res, next) => {
+  router.delete("/:id", auth, (req, res, next) => {
     Post.deleteOne({ _id: req.params.id }).then(result => {
       res.status(200).json({ message: "Post deleted!" });
     });
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,13 +1,25 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { AuthData } from "./auth-data.model";
+import { Subject } from "rxjs";
 
 @Injectable({
     providedIn:'root'
 })
 
 export class AuthService {
+    private token:string;
+    private authStatusListener = new Subject<boolean>();
+
     constructor(private http: HttpClient) {}
+
+    getToken(){
+        return this.token;
+    }
+
+    getAuthStatusListener(){
+        return this.authStatusListener.asObservable();
+    }
 
     createUser(email:string, password:string){
         const authData:AuthData = {email:email, password:password};
@@ -15,5 +27,15 @@ export class AuthService {
             .subscribe(response=>{
                 console.log(response);
             });
+    }
+
+    login(email:string, password:string){
+        const authData:AuthData = {email:email, password:password};
+        this.http.post<{token:string}>('http://localhost:3000/api/users/login',authData)
+            .subscribe(response=>{
+                const token = response.token;
+                this.token = token;
+                this.authStatusListener.next(true);
+            })
     }
 }

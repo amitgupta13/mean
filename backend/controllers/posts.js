1ccb604eba56f4e1ae9eec2adf65952450619288
Amit Gupta <vikas.s@daffodilsw.com>
2019-01-03 12:45:39 +0530

    nc

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -38,7 +38,7 @@ exports.editPost = (req, res, next)=>{
       creator:req.user.userId
     });
     Post.updateOne({_id:req.params.id, creator:req.user.userId}, post).then(result=>{
-      if(result.nModified > 0){
+      if(result.n > 0){
         res.status(200).json({message:'update successful'});
       }else{
         res.status(401).json({message:'Not Authorized'});

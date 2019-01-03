ff21ef2be35f48da7d668f9260cc86afa1cf3642
Amit Gupta <vikas.s@daffodilsw.com>
2019-01-03 15:55:33 +0530

    nc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,17 +11,12 @@ import { ErrorInterceptor } from "./error-interceptor";
 import { ErrorComponent } from "./error/error.component";
 import { AngularMaterialModule } from "./angular-material.module";
 import { PostsModule } from "./posts/posts.module";
-import { LoginComponent } from "./auth/login/login.component";
-import { SignupComponent } from "./auth/signup/signup.component";
-import { FormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ErrorComponent,
-    LoginComponent,
-    SignupComponent
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
@@ -29,8 +24,7 @@ import { FormsModule } from "@angular/forms";
     BrowserAnimationsModule,
     HttpClientModule,
     AngularMaterialModule,
-    PostsModule,
-    FormsModule
+    PostsModule
   ],
   providers: [
     {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi:true},
